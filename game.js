8b39ef2a2a9bd5b53254092e704c46ccb88b1912
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-02-16 21:06:35 -0800

    added function to get user pattern

--- game.js
+++ game.js
@@ -1,9 +1,19 @@
 var gamePattern = [];
 var buttonColours = ["red", "blue" ,"green", "yellow"];
-var randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+})
 
 function nextSequence() {
-    var randomNumber = Math.floor((Math.random()*4))
-    return randomNumber;
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/"+randomChosenColour+".mp3");
+    audio.play();
 }
