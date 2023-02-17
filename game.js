bbd3509cdfaeabdeb9e3c29e3206a10353b91e4a
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-02-16 21:12:18 -0800

    play sound when clicking btn

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var userClickedPattern = [];
 $(".btn").click(function() {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+    playSound(userChosenColour);
 })
 
 function nextSequence() {
@@ -14,6 +14,10 @@ function nextSequence() {
     gamePattern.push(randomChosenColour);
 
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
     var audio = new Audio("sounds/"+randomChosenColour+".mp3");
     audio.play();
 }
